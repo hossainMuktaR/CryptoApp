acb079219c85d2857f584a22951ddff4249a4d79
Hossain MuktaR <HossainMuktarDev@gmail.com>
2023-09-15 02:27:36 +0600

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+
+    alias(libs.plugins.com.google.dagger.hilt.android)
+    id(libs.plugins.google.devtools.ksp.get().pluginId)
+}
+
+android {
+    namespace = "com.example.cryptoapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.cryptoapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = "18"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtVersion.toString()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(platform(libs.kotlin.bom))
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.atest.ext.junit)
+    androidTestImplementation(libs.atestespresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.test.ui.junit4)
+    debugImplementation(libs.debug.ui.tooling)
+    debugImplementation(libs.debug.ui.tooling)
+
+    // Dagger-Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.hilt.android.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //    Compose dependencies
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.navigation.compose)
+//    implementation(libs.material.icons.extended)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.lifecycle.runtime.ktx)
+
+
+}
